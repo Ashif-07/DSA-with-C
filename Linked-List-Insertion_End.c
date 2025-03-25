419a6cdba433ccfa7032278032cfb8e158b88cc7
MD ASHIF ANSARI <181108726+Ashif-07@users.noreply.github.com>
2025-03-25 16:24:49 +0530

    Add files via upload

---
+++ Linked-List-Insertion_End.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void Display(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("%d -> ", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("NULL\n\n");
+}
+
+struct Node *insertAtEnd(struct Node *head, int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *p = head;
+    newNode->data = data;
+    while (p->next != NULL)
+    {
+        p = p->next;
+    }
+
+    p->next = newNode;
+    newNode->next = NULL;
+    return head;
+}
+
+int main()
+{
+    int data = 0;
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
+    struct Node *fifth;
+
+    // Allocate memory for Nodes in the linked list in heap
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+    fifth = (struct Node *)malloc(sizeof(struct Node));
+
+    head->data = 7;
+    head->next = second;
+
+    second->data = 11;
+    second->next = third;
+
+    third->data = 41;
+    third->next = fourth;
+
+    fourth->data = 66;
+    fourth->next = fifth;
+
+    fifth->data = 77;
+    fifth->next = NULL;
+    printf("Enter data you want to insert :\t");
+    scanf("%d", &data);
+    printf("\n\nBefore Insertion Linked List :  ");
+    Display(head);
+    head = insertAtEnd(head, data);
+    printf("\n\nAfter Insertion Linked List :  ");
+    Display(head);
+    return 0;
+}
