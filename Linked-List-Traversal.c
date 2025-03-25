fd573533d0747f09d555b164d44c6d117a043fee
MD ASHIF ANSARI <181108726+Ashif-07@users.noreply.github.com>
2025-03-25 12:48:42 +0530

    Add files via upload

---
+++ Linked-List-Traversal.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+void linkedListTraversal(struct node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("%d -> ", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("NULL\n\n");
+}
+
+int main()
+{
+    struct node *head;
+    struct node *second;
+    struct node *third;
+    struct node *fourth;
+    struct node *fifth;
+
+    // Allocate memory for nodes in the linked list in heap
+    head = (struct node *)malloc(sizeof(struct node));
+    second = (struct node *)malloc(sizeof(struct node));
+    third = (struct node *)malloc(sizeof(struct node));
+    fourth = (struct node *)malloc(sizeof(struct node));
+    fifth = (struct node *)malloc(sizeof(struct node));
+
+    head->data = 7;
+    head->next = second;
+
+    second->data = 11;
+    second->next = third;
+
+    third->data = 41;
+    third->next = fourth;
+
+    fourth->data = 66;
+    fourth->next = fifth;
+
+    fifth->data = 77;
+    fifth->next = NULL;
+    
+
+    printf("\n\nLinked List :  ");
+    linkedListTraversal(head);
+}
