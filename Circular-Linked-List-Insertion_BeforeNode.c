80a4adeca9c6fc256a0ac81537df3a94040d6f31
MD ASHIF ANSARI <181108726+Ashif-07@users.noreply.github.com>
2025-04-21 13:36:12 +0530

    Add files via upload

---
+++ Circular-Linked-List-Insertion_BeforeNode.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+void print(struct node *head)
+{
+    struct node *ptr = head;
+    do
+    {
+        printf("%d -> ", ptr->data);
+        ptr = ptr->next;
+    } while (ptr != head);
+    printf("(Back to Head)");
+}
+
+void insertBeforeNode(struct node*head,struct node *q, int data)
+{
+    struct node *newnode = (struct node *)malloc(sizeof(struct node));
+    newnode->data = data;
+    struct node *ptr = head;
+    struct node *p = ptr->next;
+    while(p!=q){
+        ptr=ptr->next;
+        p=p->next;
+    }
+    ptr->next = newnode;
+    newnode->next = p;
+}
+
+int main()
+{
+
+    struct node *head = (struct node *)malloc(sizeof(struct node));
+    struct node *second = (struct node *)malloc(sizeof(struct node));
+    struct node *third = (struct node *)malloc(sizeof(struct node));
+    struct node *fourth = (struct node *)malloc(sizeof(struct node));
+    struct node *fifth = (struct node *)malloc(sizeof(struct node));
+
+    head->data = 7;
+    head->next = second;
+
+    second->data = 9;
+    second->next = third;
+
+    third->data = 11;
+    third->next = fourth;
+
+    fourth->data = 13;
+    fourth->next = fifth;
+
+    fifth->data = 15;
+    fifth->next = head;
+
+    printf("\n\n");
+    print(head);
+    printf("\n\n");
+
+    insertBeforeNode(head,fifth, 999);
+
+    printf("\n\n");
+    print(head);
+    printf("\n\n");
+}
