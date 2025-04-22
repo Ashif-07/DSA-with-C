be8baf5c7c2a033ff5cbba8feee6411b99b77fc3
MD ASHIF ANSARI <181108726+Ashif-07@users.noreply.github.com>
2025-04-22 13:29:25 +0530

    Add files via upload

---
+++ Doubly-Linked-List-Insertion-Before-Node.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *prev;
+    struct node *next;
+};
+
+void printForward(struct node *head)
+{
+    struct node *ptr = head;
+    printf("\nForward Traversal: \t");
+    while (ptr != NULL)
+    {
+        printf("%d  ", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("\n\n");
+}
+
+void printBackword(struct node *end)
+{
+    struct node *ptr = end;
+    printf("Backward Traversal: \t");
+    while (ptr != NULL)
+    {
+        printf("%d  ", ptr->data);
+        ptr = ptr->prev;
+    }
+    printf("\n\n");
+}
+
+void insertBeforeNode(struct node *head, int data)
+{
+    struct node *newNode = (struct node *)malloc(sizeof(struct node));
+    struct node *ptr = head->prev;
+
+
+    newNode->data = data;
+    newNode->prev = ptr;
+    ptr->next = newNode;
+    newNode->next = head;
+    head->prev = newNode;
+}
+
+int main()
+{
+
+    struct node *head = (struct node *)malloc(sizeof(struct node));
+    struct node *second = (struct node *)malloc(sizeof(struct node));
+    struct node *third = (struct node *)malloc(sizeof(struct node));
+    struct node *fourth = (struct node *)malloc(sizeof(struct node));
+    struct node *fifth = (struct node *)malloc(sizeof(struct node));
+
+    head->prev = NULL;
+    head->data = 7;
+    head->next = second;
+
+    second->prev = head;
+    second->data = 9;
+    second->next = third;
+
+    third->prev = second;
+    third->data = 11;
+    third->next = fourth;
+
+    fourth->prev = third;
+    fourth->data = 13;
+    fourth->next = fifth;
+
+    fifth->prev = fourth;
+    fifth->data = 15;
+    fifth->next = NULL;
+
+    printForward(head);
+
+    insertBeforeNode(fifth, 23);
+
+    printForward(head);
+}
